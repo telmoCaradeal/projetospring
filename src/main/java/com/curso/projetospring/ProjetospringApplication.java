85fbfdac8e74c74064be9267bf97c5a227b392da
Telmo Cardeal <theo.cardeal@gmail.com>
2026-09-25 17:13:06 -0300

    Commit inicial do projeto Spring

---
+++ src/main/java/com/curso/projetospring/ProjetospringApplication.java
@@ -0,0 +1,15 @@
+package com.curso.projetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjetospringApplication.class, args);
+	}
+
+}
+git push -u origin main
